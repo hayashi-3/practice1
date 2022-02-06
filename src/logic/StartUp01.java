0370e1575287d3018151ff493b9bc6b7d75d7442
hayashi-3 <hayashi.gml@gmail.com>
2022-02-06 23:03:10 +0900

    first_commit

---
+++ src/logic/StartUp01.java
@@ -0,0 +1,39 @@
+package logic;
+
+import java.util.ArrayList;
+
+import dao.DBConnection;
+import dao.ProductDAO;
+import dao.ProductDTO;
+
+public class StartUp01 {
+
+	public static void main(String[] args) {
+		DBConnection db = new DBConnection();
+
+		try {
+			ProductDAO dao = new ProductDAO(db);
+			ArrayList<ProductDTO> list = dao.findAll();
+
+			if (list.size() > 0) {
+				for (ProductDTO dto : list) {
+					String id = dto.getId();
+					String name = dto.getName();
+					int price = dto.getPrice();
+
+					System.out.println(id + "¥t" + name + "¥t" + price );
+				}
+			} else {
+				System.out.println("レコードがありません。");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				db.closeConnect();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/logic/StartUp05.java
@@ -0,0 +1,31 @@
+package logic;
+
+import dao.DBConnection;
+import dao.ProductDAO;
+
+public class StartUp05 {
+
+	public static void main(String[] args) {
+		DBConnection db = new DBConnection();
+
+		try {
+			ProductDAO dao = new ProductDAO(db);
+
+			boolean judge = dao.delete("0104");
+
+			if (judge == true) {
+				System.out.println("正常に削除できました。");
+			} else {
+				System.out.println("削除に失敗しました。");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				db.closeConnect();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
